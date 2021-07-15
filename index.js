f11267292afcbe6c9878b1fda75b1544b1339469
Patrick <pdn6991@gmail.com>
2021-07-14 21:26:12 -0400

    adds babel and webpack config

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-/* global moment */
+/* global moment, mathjs */
 var moment = require('moment'); // require
+var math = require('mathjs');
 
 console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
 console.log(moment().format('dddd'));
+console.log(math.chain(3).add(4).multiply(2).multiply(2).done());
+
+var food = "pickles";
+console.log(`My favorite food is ${food}`);
