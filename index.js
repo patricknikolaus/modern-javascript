72d1a8d2f5acb27f1feb3b3573cf0397693b2e87
patricknikolaus <pdn6991@gmail.com>
2022-10-16 20:16:32 -0400

    changed num to pickles

--- index.js
+++ index.js
@@ -8,3 +8,10 @@ console.log(math.chain(3).add(4).multiply(2).multiply(2).done());
 
 var food = "pickles";
 console.log(`My favorite food is ${food}`);
+// console.log("My favorite food is " + food);
+var nums = [1, 2, 3, 4, 5, 12, 14, 16, 102];
+nums.forEach(function (num) {
+  if (num % 2 === 0) {
+    console.log("PICKLES");
+  }
+});
